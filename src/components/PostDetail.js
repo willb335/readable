6d22eade18cffe53d948d4090d0062ea2a1c6f1c
Will Bashelor <wbashelor@gmail.com>
2017-08-21 18:05:52 -0400

    Comments now in new post component

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -6,20 +6,21 @@ class PostDetail extends Component {
     return (
       <div className="user-post">
         <Well style={{ maxWidth: "50%", marginTop: "25px" }}>
-          <Panel>
-            <div>
-              <strong>Title</strong>
-            </div>
-            <div>
-              <strong>Author</strong>
-            </div>
-            <div>
-              <strong>Posted</strong>
-            </div>
-            <div>
-              <strong>Votes</strong>
-            </div>
-          </Panel>
+          {/* <Panel> */}
+          <div>
+            <strong>Title</strong>
+          </div>
+          <div>
+            <strong>Author</strong>
+          </div>
+          <div>
+            <strong>Posted</strong>
+          </div>
+          <div>
+            <strong>Votes</strong>
+          </div>
+          <hr style={{ borderWidth: "2px" }} />
+          {/* </Panel> */}
           This is where all the text will goThis is where all the text will
           goThis is where all the text will goThis is where all the text will
           goThis is where all the text will goThis is where all the text will
@@ -43,6 +44,18 @@ class PostDetail extends Component {
           text will goThis is where all the text will goThis is where all the
           text will goThis is where all the text will goThis is where all the
           text will goThis is where all the text will go
+          <hr style={{ borderWidth: "2px" }} />
+          <Panel>
+            <div>
+              Comments will be in here Comments will be in here Comments will be
+              in here Comments will be in here Comments will be in here Comments
+              will be in here Comments will be in here Comments will be in here
+              Comments will be in here Comments will be in here Comments will be
+              in here Comments will be in here Comments will be in here Comments
+              will be in here Comments will be in here Comments will be in here
+              Comments will be in here Comments will be in here
+            </div>
+          </Panel>
         </Well>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
+import PostDetail from "./components/PostDetail";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<PostDetail />, document.getElementById("root"));
 registerServiceWorker();
