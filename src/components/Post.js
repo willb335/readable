2f918d36f370e925b87135d90b9b05b128b676ca
Will Bashelor <wbashelor@gmail.com>
2017-08-21 14:07:15 -0400

    Work with flexbox

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,8 +1,37 @@
 import React, { Component } from "react";
+import { Well } from "react-bootstrap";
 
 class Post extends Component {
   render() {
-    return null;
+    return (
+      <div className="user-post">
+        <Well style={{ maxWidth: "50%", marginTop: "25px" }}>
+          This is where all the text will goThis is where all the text will
+          goThis is where all the text will goThis is where all the text will
+          goThis is where all the text will goThis is where all the text will
+          goThis is where all the text will goThis is where all the text will
+          goThis is where all the text will goThis is where all the text will
+          goThis is where all the text will goThis is where all the text will
+          goThis is where all the text will goThis is where all the text will
+          goThis is where all the text will goThis is where all the text will
+          goThis is where all the text will goThis is where all the text will
+          goThis is where all the text will goThis is where all the text will
+          goThis is where all the text will goThis is where all the text will
+          goThis is where all the text will goThis is where all the text will go
+          text will goThis is where all the text will goThis is where all the
+          text will goThis is where all the text will goThis is where all the
+          text will goThis is where all the text will goThis is where all the
+          text will goThis is where all the text will goThis is where all the
+          text will goThis is where all the text will goThis is where all the
+          text will goThis is where all the text will goThis is where all the
+          text will goThis is where all the text will goThis is where all the
+          text will goThis is where all the text will goThis is where all the
+          text will goThis is where all the text will goThis is where all the
+          text will goThis is where all the text will goThis is where all the
+          text will goThis is where all the text will go
+        </Well>
+      </div>
+    );
   }
 }
 
