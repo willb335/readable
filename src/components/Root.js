9ce5090cda9cc38fe24d6eddf6dd734df212e7a9
Will Bashelor <wbashelor@gmail.com>
2017-08-21 12:26:31 -0400

    Put down bootstrap grid

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
+import { Grid, Row, Col } from "react-bootstrap";
 
 class Root extends Component {
   render() {
-    return null;
+    return (
+      <Grid>
+        <Row className="show-category">
+          <Col xs={12} md={12}>
+            {"Filler"}
+          </Col>
+        </Row>
+      </Grid>
+    );
   }
 }
 
