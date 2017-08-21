7f4f9e19f38ecf68d881125ba7d0124762d31fe0
Will Bashelor <wbashelor@gmail.com>
2017-08-21 13:22:06 -0400

    Set up buttons for listing posts

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,10 +1,32 @@
 import React, { Component } from "react";
-import { Panel, ListGroup, ListGroupItem } from "react-bootstrap";
+import { Panel, ListGroup, ListGroupItem, Button } from "react-bootstrap";
 
 class Category extends Component {
   render() {
     return (
-      <Panel header={"Category"}>
+      <Panel
+        header={
+          <div
+            className="container"
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              flexWrap: "nowrap",
+              justifyContent: "space-between",
+              alignItems: "center"
+            }}
+          >
+            <div>Category</div>
+            <div>
+              <Button bsStyle="primary">Primary</Button>
+            </div>
+            <div style={{ float: "right" }}>
+              <Button bsStyle="primary">Primary</Button>
+            </div>
+          </div>
+        }
+        style={{ textAlign: "left" }}
+      >
         <ListGroup fill>
           <ListGroupItem>Post 1</ListGroupItem>
           <ListGroupItem>Post 2</ListGroupItem>
