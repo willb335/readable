ebb27e55a3846dd2ee72df8aefbcd62a528fef30
Will Bashelor <wbashelor@gmail.com>
2017-08-21 16:50:02 -0400

    Post body addition

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
-import { Well } from "react-bootstrap";
+import { Well, Panel } from "react-bootstrap";
 
 class Post extends Component {
   render() {
     return (
       <div className="user-post">
         <Well style={{ maxWidth: "50%", marginTop: "25px" }}>
+          <Panel>
+            <div>
+              <strong>Title</strong>
+            </div>
+            <div>
+              <strong>Author</strong>
+            </div>
+            <div>
+              <strong>Posted</strong>
+            </div>
+            <div>
+              <strong>Votes</strong>
+            </div>
+          </Panel>
           This is where all the text will goThis is where all the text will
           goThis is where all the text will goThis is where all the text will
           goThis is where all the text will goThis is where all the text will
