3a6f3710d09db4a61353dec4ef59dc4bdc8c48e2
Will Bashelor <wbashelor@gmail.com>
2017-08-23 11:27:34 -0400

    save

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,21 @@
 import { combineReducers } from "redux";
+
+import { ADD_POST, EDIT_POST, REMOVE_POST } from "../actions";
+
+function newPost(state = {}, action) {
+  switch (action.type) {
+    case ADD_POST:
+      const {
+        id,
+        timestamp,
+        title,
+        body,
+        author,
+        category,
+        voteScore,
+        deleted
+      } = action;
+  }
+}
+
+export default combineReducers({});
