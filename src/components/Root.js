009556babede985e2eb0bea1d2032521406478cc
Will Bashelor <wbashelor@gmail.com>
2017-08-23 13:58:14 -0400

    Dispatch addition to root

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { addPost, removePost, editPost } from "../actions";
 import { Grid, Row, Col } from "react-bootstrap";
 import Category from "./Category";
 
@@ -16,4 +17,35 @@ class Root extends Component {
   }
 }
 
-export default Root;
+function mapStateToProps({ food, calendar }) {
+  const dayOrder = [
+    "sunday",
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday"
+  ];
+
+  return {
+    calendar: dayOrder.map(day => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal] ? food[calendar[day][meal]] : null;
+
+        return meals;
+      }, {})
+    }))
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    addPost: data => dispatch(addPost(data)),
+    removePost: data => dispatch(removePost(data)),
+    editPost: data => dispatch(editPost(data))
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Root);
